0d6ca98eeb8ce6d3a0f2201868fc71d0e03e0283
Andreas Voit <andreas.voit@scandio.de>
2020-03-21 20:07:57 +0100

    5: Rotate google api client secrets, :shame:

--- src/main/java/de/spaceboys/medicaltaskforce/config/SecurityConfiguration.java
+++ src/main/java/de/spaceboys/medicaltaskforce/config/SecurityConfiguration.java
@@ -1,19 +1,30 @@
 package de.spaceboys.medicaltaskforce.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
+@Slf4j
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${oauth.enable}")
+    private boolean oauthEnabeled;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2Login();
+        if(oauthEnabeled) {
+            log.warn("Oauth is disabled");
+        }
+        if (oauthEnabeled) {
+            http.authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .oauth2Login();
+        }
     }
 }
