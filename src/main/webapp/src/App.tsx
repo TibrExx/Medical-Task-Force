2a78ea62abf0f32d369388ad3932e326a01c1b20
Felix Ohr <felix.ohr@scandio.de>
2020-03-21 14:52:29 +0100

    25: Added welcome screen and inital survey js form

--- src/main/webapp/src/App.tsx
+++ src/main/webapp/src/App.tsx
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 import {Button} from "@material-ui/core";
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import SignUpForm from "./components/SignUpForm";
+import WelcomeScreen from "./components/WelcomeScreen";
 
 export default function App() {
   return (
@@ -14,15 +15,12 @@ export default function App() {
       <Container maxWidth="lg">
         <Router>
           <Box my={4}>
-            <Typography variant="h5" component="h1" gutterBottom>
-              Hast du eine medizinische Ausbildung und willst Krankenhäuser im Kampf gegen Covid-19 unterstützen?
-            </Typography>
             <Switch>
               <Route path="/signup">
                 <SignUpForm />
               </Route>
               <Route path="/">
-                <Button component={Link} to="/signup" variant="contained">Ich möchte helfen!</Button>
+                <WelcomeScreen />
               </Route>
             </Switch>
           </Box>
--- src/main/webapp/src/components/SignUpForm.tsx
+++ src/main/webapp/src/components/SignUpForm.tsx
@@ -1,9 +1,160 @@
 import React from 'react'
+import {Survey} from 'survey-react';
+import Typography from "@material-ui/core/Typography";
+
+function sendDataToServer(survey: any) {
+  //send Ajax request to your web server.
+  alert("The results are:" + JSON.stringify(survey.data));
+}
+
+const surveyJson = {
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "dropdown",
+          "name": "Berufung",
+          "hasOther": true,
+          "choices": [
+            {
+              "value": "item1",
+              "text": "Altenpfleger/in"
+            },
+            {
+              "value": "item2",
+              "text": "Altenpflegehelfer/in"
+            },
+            {
+              "value": "item3",
+              "text": "Arzt"
+            },
+            {
+              "value": "item4",
+              "text": "Gesundheits- und Krankenpflegehelfer/in"
+            },
+            {
+              "value": "item5",
+              "text": "Gesundheits- und Kinderkrankenpfleger/in"
+            },
+            {
+              "value": "item6",
+              "text": "Gesundheits- und Krankenpfleger/in"
+            },
+            {
+              "value": "item7",
+              "text": "Hebamme"
+            },
+            {
+              "value": "item8",
+              "text": "Heilerziehungspfleger/in"
+            },
+            {
+              "value": "item10",
+              "text": "Notfallsanitäter/in"
+            },
+            {
+              "value": "item11",
+              "text": "Operationstechnischer Assistent/in "
+            },
+            {
+              "value": "item12",
+              "text": "Rettungsassistent/in"
+            },
+            {
+              "value": "item13",
+              "text": "Rettungssanitäter/in"
+            },
+            {
+              "value": "item14",
+              "text": "Rettungshelfer"
+            }
+          ],
+          "otherText": "Andere"
+        },
+        {
+          "type": "text",
+          "name": "Andere Berufung",
+          "visible": false,
+          "visibleIf": "{Berufung} = 'other'"
+        },
+        {
+          "type": "text",
+          "name": "Zuletzt ausgeübt am",
+          "inputType": "date"
+        },
+        {
+          "type": "matrix",
+          "name": "Verfügbarkeit",
+          "columns": [
+            {
+              "value": "available",
+              "text": "Verfügbar"
+            },
+            {
+              "value": "notAvailable",
+              "text": "Nicht verfügbar"
+            }
+          ],
+          "rows": [
+            "1-2 Dienste/Woche",
+            "2-5 Dienste/Woche",
+            "Wochenendienst",
+            "Fahrzeug"
+          ]
+        }
+      ],
+      "title": "Qualifikation und Verfügbarkeit"
+    },
+    {
+      "name": "page2",
+      "elements": [
+        {
+          "type": "text",
+          "name": "Vorname",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "Nachname",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "question3",
+          "title": "Email",
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "Telefonnummer",
+          "isRequired": true,
+          "inputType": "tel"
+        },
+        {
+          "type": "text",
+          "name": "Postleitzahl",
+          "isRequired": true,
+          "inputType": "number"
+        },
+        {
+          "type": "text",
+          "name": "Adresse"
+        }
+      ],
+      "title": "Kontaktdaten"
+    }
+  ]
+};
 
 const SignUpForm = () => {
   return(
     <div>
-      Signup form
+      <Typography variant="h5" component="h1" paragraph={true} gutterBottom>
+        Registrier dich jetzt und helfe:
+      </Typography>
+      <div id="survey" />
+      <Survey json={surveyJson} onComplete={sendDataToServer}  />
     </div>
   )
 };
