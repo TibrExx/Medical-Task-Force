c16db3d5d8677930648b266737e0b68f0fe6cb01
Felix Ohr <felix.ohr@scandio.de>
2020-03-22 13:08:31 +0100

    49: fixed redux extension bug

--- src/main/webapp/src/store/index.ts
+++ src/main/webapp/src/store/index.ts
@@ -1,8 +1,10 @@
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import createRootReducer from "./reducers/rootReducer";
 import thunk from 'redux-thunk';
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== undefined ?
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
 
 const store = createStore(createRootReducer(), composeEnhancers(applyMiddleware(thunk)));
 
