bbf73e0882611e991366f7920f5d5fd196458f89
Felix Ohr <felix.ohr@scandio.de>
2020-03-22 11:51:38 +0100

    Added redux and google oauth

--- src/main/webapp/src/config/index.ts
+++ src/main/webapp/src/config/index.ts
@@ -1,5 +1,6 @@
 
 const apiUrl = 'http://prod.eba-pipszcdb.eu-central-1.elasticbeanstalk.com';
 const basePath = '/api/v1';
+const googleClientId = '87896894959-9inbopi0v3cebs1rktlpk0vb70qd4vvu.apps.googleusercontent.com';
 
-export { apiUrl, basePath };
+export { apiUrl, basePath, googleClientId };
